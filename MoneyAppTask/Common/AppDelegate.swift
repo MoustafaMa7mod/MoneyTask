41b59246fcd92fd68cde3479c68e343d4321795a
MostafaMahmoudMohamed <m.mahmoud@nmowpay.cash>
2021-02-09 20:09:19 +0200

    add structure group to project

---
+++ MoneyAppTask/Common/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MoneyAppTask
+//
+//  Created by NowPay on 2/9/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
--- MoneyAppTask/Controllers/Authentication/View/AuthenticationViewController.swift
+++ MoneyAppTask/Controllers/Authentication/View/AuthenticationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AuthenticationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
