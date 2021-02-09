73fb3efa00dcfc2ccc2d4422cd7265648f798ad9
MostafaMahmoudMohamed <m.mahmoud@nmowpay.cash>
2021-02-09 19:54:01 +0200

    Init commit

---
+++ MoneyAppTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyAppTask
+//
+//  Created by NowPay on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
